1e1b7ebc81e3465bdb9e6e6b7d7ccbff0700648a
Rajesh <86980094+rajesh0125@users.noreply.github.com>
2026-03-11 19:22:07 +0530

    css update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "rajeshtech.onrender.com",
-    ".onrender.com",
     "rajeshtech.online",
     "www.rajeshtech.online"
 ]
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
